1796455d70e877cc82a78ca899f386bcb4f4ed4c
B1anker <854889302@qq.com>
2016-09-24 15:12:28 +0800

    update web and test the event on my phone

--- js/main.js
+++ js/main.js
@@ -4,4 +4,21 @@
 
 $(document).ready(function(){
 	$(document.body).css('height', $(document).height());
+
+
+	var touchDown,touchUp;
+	$(document).on('touchstart', function(e){
+		e = e || window.event;
+		touchDown = e.originalEvent.changedTouches[0];
+	}).on('touchend', function(e){
+		e = e || window.event;
+		touchUp = e.originalEvent.changedTouches[0];
+		if(touchUp.pageY > touchDown.pageY){
+			alert('up');
+		}else if(touchUp.pageY < touchDown.pageY){
+			alert('down');
+		}else{
+			alert('click');
+		}
+	})
 });

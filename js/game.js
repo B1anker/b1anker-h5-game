0e95ff7782fcd89affa470041e69d14ad56d9700
B1anker <854889302@qq.com>
2016-09-26 21:35:35 +0800

    fix event bug
    when the game finished,I should drop the event
    
    {$(‘#again').off('touchstart')   in
    GamePage.prototype.interval}

--- js/game.js
+++ js/game.js
@@ -15,14 +15,16 @@
 		this.canvas.height = $(document).height();
 	};
 
-
+	//随机产生指定范围的整数
 	GamePage.prototype.randomNumber = function(min, max){
 		return Math.floor(Math.random() * (max - min + 1) + min);
 	};
 
-
+	//怪物存放
 	var monsters = {};
 
+
+	//创建怪物对象
 	GamePage.prototype.createMonster = {
 		mon1: function (option) {
 			var img = new Image();
@@ -62,9 +64,11 @@
 		}
 	};
 
+	//怪物死亡图片的大小
 	GamePage.prototype.die = [[180, 138],[146, 84],[103, 94],[92, 107]];
 
 
+	//创建每关怪物的数量
 	GamePage.prototype.createMonsterQuantity = function(){
 		var self = this;
 		for (var i = 0; i < self.level; i++) {
@@ -78,26 +82,32 @@
 		}
 	};
 
+
+
+	//绘制怪物
 	GamePage.prototype.drawMonsters = function(){
 		var self = this;
 		var i = 0;
 		var timer = null;
-		clearInterval(timer);
+
 		timer = setInterval(function(){
-			var id = 'mon' + i + self.turns;
-			new self.createMonster['mon' + self.randomNumber(1,4)]({
-				canvas: self.settings.canvas,
-				id: id
-			});
-			self.monsterMove(id);
-			i++;
-			if(i >= self.monsterStore[self.turns]){
+			if(i >= self.monsterStore[self.turns] || self.gameOver){
 				clearInterval(timer);
+			}else{
+				//console.log('第' + (self.turns + 1) + '关-第' + (i + 1) +'只怪怪物');
+				var id = 'mon' + i + self.turns;
+				new self.createMonster['mon' + self.randomNumber(1,4)]({
+					canvas: self.settings.canvas,
+					id: id
+				});
+
+				self.monsterMove(id);
+				i++;
 			}
 		}, 300);
 	};
 
-
+	//生成每次怪物运动的位置
 	GamePage.prototype.generatePosition = function(){
 		var self = this;
 		var x,y;
@@ -126,6 +136,8 @@
 		return {x:x,y:y};
 	};
 
+
+	//怪物运动
 	GamePage.prototype.run = function(position, id, radiu, angle){
 		var self = this;
 		if(position.x <= self.max.x && position.x > self.min.x && position.y == self.min.y){
@@ -163,7 +175,7 @@
 	};
 
 
-
+	//杀死怪物
 	GamePage.prototype.killMonster = function(id, timer, pX, pY){
 		clearInterval(timer);
 		var self = this;
@@ -180,6 +192,7 @@
 			};
 			//点击怪物死亡
 			jc('#' + id).del();
+			monsters[id] = undefined;
 			//死亡图片消失特效
 			var disappear = 1;
 			var disappearTimer = setInterval(function(){
@@ -190,41 +203,47 @@
 				}
 				jc('#die' + id).opacity(disappear);
 			},30);
-			monsters[id] = undefined;
 		}
 		for(var i in monsters){
 			if(monsters[i]){
 				return false;
 			}
 		}
-		self.turns++;
-		self.next();
+		if(!self.gameOver){
+			self.turns++;
+			self.next();
+		}
 	};
 
+
+	//下一关
 	GamePage.prototype.next = function(){
 		var self = this;
-		monsters = {};
-		self.position = {};
-		$(document).off('touchstart');
-		self.interval();
-		self.drawMonsters();
+		if(!self.gameOver){
+			self.position = {};
+			$(document).off('touchstart');
+			self.interval();
+			self.drawMonsters();
+		}
 	};
 
+
+	//计时,每关X秒,通过重置时间
 	GamePage.prototype.interval = function(){
-		var self = this;
-		var leftTime = self.time;
+		var self = this,
+			leftTime = self.time;
 		clearTimeout(self.gameOverTimer);
 		clearInterval(self.timeTimer);
 		self.timeTimer = setInterval(function(){
 			leftTime--;
 			$('.time').html(leftTime + 'S');
 		}, 1000);
-
+		//游戏结束
 		self.gameOverTimer = setTimeout(function(){
+			self.gameOver = true;
 			clearInterval(self.timeTimer);
 			for(var a in monsters){
-				jc('#' + a).del();
-				monsters[a] = null;
+				delete a;
 			}
 			self.score = 0;
 			self.turns = 0;
@@ -232,39 +251,45 @@
 			self.position = {};
 			$(document).off('touchstart');
 			$('.final-score').html('你消灭了' + $('.score').html().match(/\d+/)[0] + '只怪物!');
+			$('.score').html('X0');
+			$('.time').html('10S');
 			$('.alert').show('slow');
-			$('#again').on('touchstart', function(){
+			$('#again').on('touchstart', function(e){
 				$('.alert').hide('slow');
+				self.gameOver = false;
 				self.next();
+				$('#again').off('touchstart');
 			});
 		}, self.time * 1000);
 	};
 
-
+	//给document绑定click事件来模拟点击怪物
 	GamePage.prototype.bindTouch = function(id, timer){
 		var self = this;
-			$(document).on('touchstart', function(e){
-				if(self.position[id]) {
-					e = e || window.event;
-					var touchPosition = e.originalEvent.changedTouches[0],
-						tX = touchPosition.pageX,
-						tY = touchPosition.pageY,
-						pX = self.position[id][0],
-						pY = self.position[id][1];
-					if ((pX <= tX - 10) && tX <= (pX + 120) && (pY - 10) <= tY && tY <= (pY + 120)) {
-						self.killMonster(id, timer, pX, pY);
-						self.score++;
-						$('.score').html('X' + self.score);
-					}else{
-						$('.bg-color').addClass('shake');
-						setTimeout(function(){
-							$('.bg-color').removeClass('shake');
-						}, 1000);
-					}
+		$(document).on('touchstart', function(e){
+			if(self.position[id]) {
+				e = e || window.event;
+				var touchPosition = e.originalEvent.changedTouches[0],
+					tX = touchPosition.pageX,
+					tY = touchPosition.pageY,
+					pX = self.position[id][0],
+					pY = self.position[id][1];
+				if ((pX <= tX - 10) && tX <= (pX + 120) && (pY - 10) <= tY && tY <= (pY + 120)) {
+					self.killMonster(id, timer, pX, pY);
+					self.score++;
+					$('.score').html('X' + self.score);
+				}else{
+					$('.bg-color').addClass('shake');
+					setTimeout(function(){
+						$('.bg-color').removeClass('shake');
+					}, 1000);
 				}
-			});
+			}
+		});
 	};
 
+
+	//怪物移动
 	GamePage.prototype.monsterMove = function(id){
 		var self = this;
 		var position = self.generatePosition();
@@ -272,13 +297,17 @@
 		var angle = self.randomNumber(0, 360);
 		var lastLocation;
 		var timer = setInterval(function(){
-			angle += self.speed;
-			if(angle >= 360){
-				angle = 0;
+			if(self.gameOver){
+				jc('#' + id).del();
+				clearInterval(timer);
+			}else{
+				angle += self.speed;
+				if(angle >= 360){
+					angle = 0;
+				}
+				lastLocation = self.run(position, id, radiu ,angle);
 			}
-			lastLocation = self.run(position, id, radiu ,angle);
 		}, self.timerSpeed);
-
 		self.bindTouch(id, timer);
 	};
 
@@ -300,16 +329,21 @@
 			x: self.width - 120,
 			y: self.height - 160
 		};
-		self.time = 5;
+		self.time = 10;
 		self.roundTime = 10000;
 		self.timerSpeed = 30;
 		self.speed = 6;
 		self.position = {};
+		self.gameOver = false;
 		self.distance = (self.max.x - self.min.x + self.max.y - self.min.y) * 2;
 		self.step = self.distance / (self.roundTime / self.timerSpeed);
 		self.createMonsterQuantity();
-		self.drawMonsters();
-		self.interval();
+		$('#begin').on('touchstart', function(){
+			$('.active').hide('slow');
+			self.drawMonsters();
+			self.interval();
+		});
+
 	};
 
 	//扩展jQuery
